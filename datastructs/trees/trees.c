570a103873de40454350b5d29ddf805860f81bd8
jcbiznoff <hyunjinc.chung@gmail.com>
2014-01-27 13:06:37 +0900

    tree api updated

--- datastructs/trees/trees.c
+++ datastructs/trees/trees.c
@@ -22,7 +22,6 @@ node * build123(){
  *            /
  *           3
  * */
-
 node * build1234(){
     node *a = newNode(2);
     a->left = newNode(1);
@@ -30,26 +29,38 @@ node * build1234(){
     a->right->left = newNode(3);
     return a;
 }
-
+node *buildComplex(){
+    node *a = newNode(4);
+    a->left = newNode(1);
+    a->right = newNode(5);
+    a->left->right = newNode(3);
+    a->left->right->left = newNode(2);
+    a->right->right = newNode(10);
+    a->right->right->left = newNode(7);
+    a->right->right->right = newNode(12);
+}
 void print123(node *a){
     printf("  %d\n",a->data);
     printf(" / \\\n");
     printf("%d   %d\n",a->left->data, a->right->data);
 }
+void deleteAllNodes(node * nd){
+    if(nd == NULL) return;
 
-int getHeight(node * nd){
-    int a = 0;
-    int b =0;
-
-    if(nd == NULL) return 0;
-
-    a = getHeight(nd->left);
-    b = getHeight(nd->right);
-
-    if(a>b) return a+1;
-    return b+1;
+    deleteAllNodes(nd->left);
+    deleteAllNodes(nd->right);
+    free(nd);
 }
-
+void insertNodeBST(node *nd, int x){
+    if(nd == NULL) {
+        nd = newNode(x);
+        return;
+    }
+    if(x <= nd->data)
+        insertNodeBST(nd->left, x);
+    else
+        insertNodeBST(nd->right,x);
+}
 void printInorderTraverse(node * nd){
     if(nd == NULL) return;
     printInorderTraverse(nd->left);
@@ -68,7 +79,12 @@ void printPreorderTraverse(node *nd){
     printPreorderTraverse(nd->left);
     printPreorderTraverse(nd->right);
 }
-
+void printLevelorderTraverse(node *nd){
+    int i=0;
+    for(i=0; i<getHeight(nd);i++){
+        printLevelHelper(nd,i);
+    }
+}
 void printLevelHelper(node *nd, int height){
     if (nd == NULL) return;
     if(height == 1) printf("%d ", nd->data);//leaf case
@@ -76,15 +92,83 @@ void printLevelHelper(node *nd, int height){
     printLevelHelper(nd->left, height-1);
     printLevelHelper(nd->right, height -1);
 }
-void printLevelorderTraverse(node *nd){
-    int i=0;
-    for(i=0; i<getHeight(nd);i++){
-        printLevelHelper(nd,i);
-    }
+int getHeight(node * nd){
+    int a = 0;
+    int b =0;
+
+    if(nd == NULL) return 0;
+
+    a = getHeight(nd->left);
+    b = getHeight(nd->right);
+
+    if(a>b) return a+1;
+    return b+1;
 }
 int getNodeNum(node *nd){
     if(nd == NULL) return 0;
     return getNodeNum(nd->left) + 1 + getNodeNum(nd->right);
+}
+void printR2Lpath(node *nd){
+    int arr[1000];
+    printR2LpathHelper(nd,  arr, 0);
+}
+void printR2LpathHelper(node *nd, int arr[], int len){
+    if(nd == NULL) return;
+
+    if(nd->left == NULL && nd->right == NULL)
+        printArray(arr,len);
+    arr[len++] = nd->data;
+    printR2LpathHelper(nd->left, arr, len+1);
+    printR2LpathHelper(nd->right, arr, len+1);
+}
+int hasPathSum(node *nd, int sum){
+    if (nd == NULL) return (sum == 0); //base case
+
+    sum = sum - nd->data;
+    return hasPathSum(nd->left, sum) || //recursive case
+           hasPathSum(nd->right,sum);
+}
+/* doubleTree: duplictes each node and attach to left
+ * */
+void doubleTree(node *nd){
+    if(nd == NULL) return;
+    doubleTree(nd->left);
+    doubleTree(nd->right);
+
+    node *tmp = nd->left;
+    nd->left = newNode(nd->data);
+    nd->left->left = tmp;
+}
+/*mirrorTree : "swaps" each left and right nodes of a tree
+ *
+ */
+void mirrorTree(node *nd){
+    if(nd==NULL) return;
+
+    mirrorTree(nd->left);
+    mirrorTree(nd->right);
+    node *tmp = nd->right;
+    nd->right = nd->left;
+    nd->left= tmp;
+}
+/* countPossibleTrees(int n):
+ * all possible formation of a BST with a given number of nodes
+ *  bstn = catalan # = (2n)! / (n+1)!n!
+ *
+ *  this implementation relies on a recursive formulation for the above
+ *  b(n) = sum(b(i-1)*b(n-i)) where i=1 to n
+ *
+ * */
+int countPossibleTrees(int n){
+    if(n == 0 || n ==1) return 1;
+
+    int sum = 0;
+    int i;
+    for(i=1; i<=n; i++){
+        sum = sum + countPossibleTrees(i-1)*countPossibleTrees(n-i);
+    }
+
+    return sum;
 }
 int areTreesIdentical(node *a, node *b){
     if(a == NULL && b == NULL) return 1;
@@ -96,51 +180,39 @@ int areTreesIdentical(node *a, node *b){
     }
     return 0;
 }
-
-void deleteAllNodes(node * nd){
-    if(nd == NULL) return;
-
-    deleteAllNodes(nd->left);
-    deleteAllNodes(nd->right);
-    free(nd);
+int getMinValBST(node *nd){
+    node * cur = nd;
+    while(cur != NULL){
+        cur = cur->left;
+    }
+    return cur->data;
 }
-
-void mirrorTree(node *nd){
-    if(nd==NULL) return;
-
-    mirrorTree(nd->left);
-    mirrorTree(nd->right);
-    node *tmp = nd->right;
-    nd->right = nd->left;
-    nd->left= tmp;
+int getMaxValBST(node *nd){
+    node *cur = nd;
+    while(cur != NULL){
+        cur = cur->right;
+    }
+    return cur->data;
 }
+int isBST1(node *nd){
+    if(nd == NULL) return 1;
+
+    if(nd->left != NULL && getMaxValBST(nd->left) > nd->data)
+        return 0;
+
+    if(nd->right != NULL && getMaxValBST(nd->right) <= nd->data)
+        return 0;
+
+    if(!isBST1(nd->left) || !isBST1(nd->right))
+        return 0;
+
+    return 1;
+}
+int isBST2(node *nd){}
 void printArray(int arr[], int len){
     int i;
     for(i=0; i<len; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
-}
-void printR2LpathHelper(node *nd, int arr[], int len){
-    if(nd == NULL) return;
-
-    if(nd->left == NULL && nd->right == NULL)
-        printArray(arr,len);
-    arr[len++] = nd->data;
-    printR2LpathHelper(nd->left, arr, len+1);
-    printR2LpathHelper(nd->right, arr, len+1);
-
-}
-void printR2Lpath(node *nd){
-    int arr[1000];
-    printR2LpathHelper(nd,  arr, 0);
-}
-
-int getMinValBST(node *nd){
-    node * cur = nd;
-    while(cur != NULL){
-        cur = cur->left;
-    }
-
-    return cur->data;
 }
--- datastructs/trees/trees.h
+++ datastructs/trees/trees.h
@@ -4,3 +4,90 @@ typedef struct _node{
     int data;
 }node;
 
+//Tree base operations
+node *newNode(int x);
+node *build123();
+node *build1234();
+void print123();
+
+//Dictionary Operation
+void deleteAllNodes(node *nd);
+void insertNodeBST(node *nd, int x);
+
+
+//Traversals
+void printInorderTraverse(node *nd);
+void printPostorderTraverse(node *nd);
+void printPreorderTraverse(node *nd);
+void printLevelorderTraverse(node *nd);
+void printLevelHelper(node *nd, int lvl);
+void printSpiralLevelorder(node * nd); //to do
+void printSpiralHelper(node *nd, int lvl);//to do
+void printReverseLevelTraverse(node *nd);
+
+//Apis
+int getHeight(node*nd);
+int getNodeLvl(node *nd,int k);// to do
+int getNodeNum(node *nd);
+void printR2Lpath(node *nd);
+void printR2LpathHelper(node *nd, int arr[], int len);
+int hasPathSum(node*nd, int sum);
+void doubleTree(node *nd);
+void mirrorTree(node *nd);
+
+
+int getDiameter(node *nd);//to do
+int getTreeMaxWidth(node *nd);//to do
+int isFoldable(node *nd); // to do
+void printNodesOneLevel(node *nd, int lvl);
+void printAcestor(node *nd, int k);
+void printGivenRangeBST(node *nd, int n1, int n2);
+int isBSTSumTree(node* nd);
+int isSubTree(node *nd1, node *nd2);
+void convert2SumTree(node *nd1);
+int largestBST(node *nd);
+void printVerticalSum(node *nd);
+int maxSumR2L(node *nd);
+int binTree2BST(node*nd);
+int isSingleChildTree(node *nd);
+int isTreeComplete(node *nd);
+void printBoundaryTraverse(node *nd);
+void swap2Incorrect(node *nd);
+void buildTreePreandPost(node *nd);
+int getCeil(node *nd, int k);
+void toBSTtoBin(node *); //add all nodes greater than current to val
+int sumExists(node *, int sum);
+void linklist2BinTree(node*); //convert a link list to bin tree
+void removeNodesInRange(node *nd, int min, int max);
+void binTree2DoubleLinkList(node *);
+int isIsomorphic(node *n1, node *n2);
+void removeNodesSumLessthanK(node *n1, int sum);
+int getDeepestLeftNode(node *n1);
+int findNextRightNode(node *n1, int k);
+
+int getHeightIter(node *n);
+void printCustomTree(node *n);
+int diffOddEvenLvl(node *n);
+int getDeepestOddLvl(node *n, int lvl);
+int leavesSameLevel(node *n);
+void printLeftNodesAllLevel(node *n);
+
+
+
+int doesNodeExist(node *nd, int x);
+int countPossibleTrees(int numberm);
+
+//Two trees
+int areTreesIdentical(node *nd1, node *nd2);
+
+//BST only
+int getMinValBST(node *nd);
+int isBST1(node * nd);
+int isBST2(node *nd);
+
+int isHeightBalancedBST(node *nd);// to do
+int getKthSmallets(node *nd, int k)// to do
+
+//Array Helper funcitons
+void printArray(int arr[], int len);
+
