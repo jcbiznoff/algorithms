d3665e0cf275218b02a90fee6ed81f7be5c742b2
jcbiznoff <hyunjinc.chung@gmail.com>
2014-02-03 20:24:41 +0900

    updated directories

---
+++ strings/copystr.c
@@ -0,0 +1,48 @@
+/*
+ * given two pointers to string, copy one from the other
+ *
+ * void cpystr(char *str1, char *str2)
+ *
+ * modify a string within a function
+ * void modstr2(char **str1)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void modstr2(char (*str1)[]){
+    char * mystr = *str1;
+    mystr[0]= 'M';
+}
+void cpystr(char *str1, char *str2){
+    while(*str2 != 0){
+        *str1++ = *str2++;
+    }
+}
+char *cpystrInside(char *str1){
+    char *tmpstr;
+    int i; //use index. Not wise to manipulate pointer of tmpstr and return tmpstr
+    tmpstr = (char*)malloc(strlen(str1)+1);
+    for(i=0; i<=strlen(str1); i++)
+        tmpstr[i] = str1[i];
+
+    printf("tmpstr: %s\n",tmpstr);
+    return tmpstr;
+}
+
+int main(){
+    //char *a= "abcd";//readonly!!!!!!!
+    char a[10] = "abcd"; //array eanbles read/write
+    char b[10] = "kbsdf";
+
+    char *c1 = "bleasdf";// read only
+    char *c2;
+    printf("%s\n",a);
+    //modstr2(&a);
+    //cpystr(a,b);
+
+    c2 = cpystrInside(c1);
+    printf("%s\n",c2);
+    free(c2);
+    return 0;
+}
+
---
+++ strings/splitstr.c
@@ -0,0 +1,9 @@
+/*
+ * given a string separated with spaces, split into buffers
+ *
+ * "car boat ship car"
+ *
+ * copyStr2array
+ */
+
+
